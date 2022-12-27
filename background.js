627dfddfe0ccd9983e0a05dde2617fb665fae64e
BenjaminHalko <benjaminhalko@hotmail.com>
2022-12-27 13:31:50 -0800

    Added Lively Wallpaper Settings

--- background.js
+++ background.js
@@ -1,17 +1,18 @@
 // Variables
+var numCircles = 8;
 
-const numCircles = 8;
+var frameRate = 60;
 
-const minHue = 190;
-const maxHue = 285;
+var minHue = 190;
+var maxHue = 285;
 
-const spd = 0.0025;
+var spd = 0.05;
 
-const startRadius = 200;
-const endRadius = 500;
+var startRadius = 200;
+var endRadius = 500;
 
 // ** DO NOT EDIT BELOW THIS LINE **
-
+var spdReal = spd / frameRate;
 // Setup the canvas
 document.body.style.background = "black";
 document.body.innerHTML = `<canvas id='canvas' style='position:fixed;left:0;top:0;z-index:-1'></canvas>
@@ -30,18 +31,12 @@ for (var i = 0; i < numCircles; i++) {
     });
 }
 
-
-animatedBackground();
-function animatedBackground() {
-    var grd;
+const animate = function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-
-    // Clear the canvas
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
     
     for(var i = 0; i < circles.length; i++) {
-        circles[i].r += spd;
+        circles[i].r += spdReal;
 
         if (circles[i].r >= 1) {
             circles[i].r = 0;
@@ -50,13 +45,49 @@ function animatedBackground() {
             circles[i].color = minHue + Math.round(Math.random() * (maxHue - minHue));
         }
 
-        grd = ctx.createRadialGradient(circles[i].x, circles[i].y, 0, circles[i].x, circles[i].y, Math.round(startRadius + Math.sin(circles[i].r * Math.PI) * (endRadius - startRadius)));
-        grd.addColorStop(0, 'hsla(' + circles[i].color + ', 100%, 15%, '+Math.round(Math.min(100, (1-Math.abs(1-circles[i].r*2))*140))+'%)');
-        grd.addColorStop(1, 'hsla(' + circles[i].color + ', 100%, 15%, 0)');
-
-        ctx.fillStyle = grd;
+        ctx.fillStyle = ctx.createRadialGradient(circles[i].x, circles[i].y, 0, circles[i].x, circles[i].y, Math.round(startRadius + Math.sin(circles[i].r * Math.PI) * (endRadius - startRadius)));
+        ctx.fillStyle.addColorStop(0, 'hsla(' + circles[i].color + ', 100%, 15%, '+Math.round(Math.min(100, (1-Math.abs(1-circles[i].r*2))*140))+'%)');
+        ctx.fillStyle.addColorStop(1, 'hsla(' + circles[i].color + ', 100%, 15%, 0)');
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
+}
 
-    setTimeout(animatedBackground, 30);
+var interval = setInterval(animate, 1000 / frameRate);
+
+function recolor() {
+    for (var i = 0; i < circles.length; i++) {
+        circles[i].color = minHue + Math.round(Math.random() * (maxHue - minHue));
+    }
+}
+
+function livelyPropertyListener(name, val) {
+    switch(name) {
+        case "numCircles":
+            numCircles = val;
+            break;
+        case "frameRate":
+            frameRate = val;
+            spdReal = spd / frameRate;
+            clearInterval(interval);
+            interval = setInterval(animate, 1000 / frameRate);
+            break;
+        case "minHue":
+            minHue = val;
+            recolor();
+            break;
+        case "maxHue":
+            maxHue = val;
+            recolor();
+            break;
+        case "spd":
+            spd = val / 200;
+            spdReal = spd / frameRate;
+            break;
+        case "startRadius":
+            startRadius = val;
+            break;
+        case "endRadius":
+            endRadius = val;
+            break;
+    }
 }
